3a27eea5f8989156a4f073bc114286b4fd9ea467
Lourival Linard <l.linard@gmail.com>
2019-03-01 12:18:39 -0300

    Observable ValueChanges de Reactive Forms

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {trigger, state, style, transition, animate} from '@angular/animations'
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms'
 
 import {Restaurant} from './restaurant/restaurant.model'
 import { RestaurantsService } from './restaurants.service';
+
+import 'rxjs/add/operator/switchMap'
 
 @Component({
   selector: 'mt-restaurants',
@@ -25,12 +28,30 @@ import { RestaurantsService } from './restaurants.service';
 export class RestaurantsComponent implements OnInit {
 
   searchBarState = 'hidden'
-
   restaurants: Restaurant[]
 
-  constructor(private restaurantsService: RestaurantsService) { }
+  searchForm: FormGroup
+  searchControl: FormControl
+
+  constructor(private restaurantsService: RestaurantsService,
+              private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.searchControl = this.fb.control('')
+    this.searchForm = this.fb.group({
+      searchControl: this.searchControl
+    })
+
+    this.searchControl.valueChanges
+    //.debounceTime(500)
+    //.distinctUntilChanged()
+    .switchMap(searchTerm =>
+      this.restaurantsService
+        .restaurants(searchTerm))
+       // .catch(error=>Observable.from([])))
+      .subscribe(restaurants => this.restaurants = restaurants)
+
     this.restaurantsService.restaurants().subscribe(restaurants => this.restaurants = restaurants)
   }
 
