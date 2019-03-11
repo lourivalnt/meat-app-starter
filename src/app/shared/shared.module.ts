6e201d2cba6ed049fa5c664422d816da9a91570f
Lourival Linard <l.linard@gmail.com>
2019-03-11 09:51:09 -0300

    Implementando Route Guard CanDeactivate na Compra

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import {NgModule, ModuleWithProviders} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {HTTP_INTERCEPTORS} from '@angular/common/http'
 
 import {InputComponent} from './input/input.component'
 import {RadioComponent} from './radio/radio.component'
@@ -12,8 +13,10 @@ import {RestaurantsService} from '../restaurants/restaurants.service';
 import { SnackbarComponent } from './messages/snackbar/snackbar.component';
 
 import {NotificationService} from './messages/notification.service'
-import { LoginService } from 'app/security/login/login.service';
-import { LoggedInGuard } from 'app/security/loggedin.guard';
+import {LoginService} from '../security/login/login.service'
+
+import {LoggedInGuard} from '../security/loggedin.guard'
+import {LeaveOrderGuard} from '../order/leave-order.guard'
 
 @NgModule({
   declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
@@ -26,12 +29,14 @@ export class SharedModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: SharedModule,
-      providers:[ShoppingCartService, 
-                 RestaurantsService, 
-                 OrderService, 
-                 NotificationService, 
+      providers: [ShoppingCartService,
+                 RestaurantsService,
+                 OrderService,
+                 NotificationService,
                  LoginService,
-                 LoggedInGuard]
+                 LoggedInGuard,
+                 LeaveOrderGuard
+                ]
     }
   }
 }
